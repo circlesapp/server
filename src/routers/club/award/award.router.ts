07ec9532c5f7888ec47e46aa53acb790fc6d198e
Andy0414 <pjh8667@naver.com>
2019-10-24 09:52:03 +0900

    UPDATE award router

--- src/routers/club/award/award.router.ts
+++ src/routers/club/award/award.router.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import PassportJWTAuth from "../../../modules/PassportJWT-Auth";
-import { GetPublicAwards, Write } from "./award.controller";
+import { GetPublicAwards, Write, Delete } from "./award.controller";
 
 const router = Router();
 
 // Routers
-router.post("/write",PassportJWTAuth.authenticate(),Write)
+router.post("/write", PassportJWTAuth.authenticate(), Write);
+router.post("/delete", PassportJWTAuth.authenticate(), Delete);
 router.get("/getPublicAwards", GetPublicAwards);
 
 export default router;
