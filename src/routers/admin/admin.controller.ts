ea8c645eae92e2f2271a1c7cc2dcb13ff12010fd
Andy0414 <pjh8667@naver.com>
2019-11-25 09:32:03 +0900

    UPDATE testPush

--- src/routers/admin/admin.controller.ts
+++ src/routers/admin/admin.controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response, NextFunction } from "express";
+import PushManager from "../../modules/Push-Manager";
 
 export const Page = (req: Request, res: Response, next: NextFunction) => {
 	res.sendFile("public/admin/index.html");
 };
 export const TestPushAlarm = (req: Request, res: Response, next: NextFunction) => {
+	PushManager.sendMessage(req.body, "PushTest");
 };
