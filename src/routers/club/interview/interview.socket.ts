c86c863ece653e1df3c697d329c89a8d7bb2a55d
Andy0414 <pjh8667@naver.com>
2019-12-18 11:55:14 +0900

    UPDATE io.sokets all

--- src/routers/club/interview/interview.socket.ts
+++ src/routers/club/interview/interview.socket.ts
@@ -30,8 +30,10 @@ class InterviewRoomManager {
 	checkRedundancy(clubname: string) {
 		return this.interviewRooms.findIndex(interviewRoom => interviewRoom.clubname == clubname) == -1;
 	}
-	startInterview(clubname: string, interviewers: Interviewer[]): void {
-		this.interviewRooms.push(new InterviewRoom(clubname, interviewers));
+	startInterview(clubname: string, interviewers: Interviewer[]): InterviewRoom {
+		let interview = new InterviewRoom(clubname, interviewers);
+		this.interviewRooms.push(interview);
+		return interview;
 	}
 	closeInterview(clubname: string): void {
 		this.interviewRooms.splice(
@@ -50,14 +52,14 @@ const socketRouter: SocketRouter = (io: SocketIO.Server, socket: SocketIO.Socket
 		if (!interviewRoomManager.checkRedundancy(data.clubname)) {
 			socket.emit("interview_startInterview", { result: false, message: "이미 시작된 면접입니다." } as InterviewResponse);
 		} else {
-			interviewRoomManager.startInterview(data.clubname, data.interviewers);
-			socket.emit("interview_startInterview", { result: true, message: "면접 생성 성공" } as InterviewResponse);
+			let interview = interviewRoomManager.startInterview(data.clubname, data.interviewers);
+			io.sockets.in(data.clubname).emit("interview_startInterview", { result: true, message: "면접 생성 성공", data: interview } as InterviewResponse);
 		}
 	});
 	socket.on("interview_closeInterview", (data: clubnameRequest) => {
 		if (!interviewRoomManager.checkRedundancy(data.clubname)) {
 			interviewRoomManager.closeInterview(data.clubname);
-			io.sockets.in(data.clubname).emit("interview_closeInterview", { result: true, message: "면접이 끝났습니다." } as InterviewResponse);
+			io.sockets.in(data.clubname).emit("interview_closeInterview", { result: true, message: "면접 종료 성공" } as InterviewResponse);
 		} else {
 			socket.emit("interview_closeInterview", { result: false, message: "면접이 없습니다." } as InterviewResponse);
 		}
