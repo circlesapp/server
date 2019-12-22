1ebf7e77f99a331bed4ad5419dc3dfc8130e7779
Andy0414 <pjh8667@naver.com>
2019-12-22 17:02:35 +0900

    UPDATE attendance members

--- src/routers/club/attendance/atendance.socket.ts
+++ src/routers/club/attendance/atendance.socket.ts
@@ -3,18 +3,17 @@ import * as SocketIO from "socket.io";
 import Club from "../../../schemas/Club";
 import Logger from "../../../modules/Logger";
 
-type clubnameRequest = { clubname: string; interviewers?: Interviewer[] };
-type InterviewResponse = { result: boolean; message: string; data?: any };
-type Interviewer = { name: string; number: string };
+type clubnameRequest = { clubname: string; members?: any[] };
+type AttendanceResponse = { result: boolean; message: string; data?: any };
 class Attendance {
 	clubname: string;
-	interviewers: Interviewer[];
-	constructor(clubname, interviewers) {
+	members: any[];
+	constructor(clubname, members) {
 		this.clubname = clubname;
-		this.interviewers = interviewers;
+		this.members = members;
 	}
-	setInterviewers(interviewers: Interviewer[]) {
-		this.interviewers = interviewers;
+	setMembers(members: any[]) {
+		this.members = members;
 	}
 }
 
@@ -28,21 +27,21 @@ class AttendanceManager {
 	 * @param clubname 동아리 이름
 	 */
 	checkRedundancy(clubname: string) {
-		return this.attendances.findIndex(interviewRoom => interviewRoom.clubname == clubname) == -1;
+		return this.attendances.findIndex(attendance => attendance.clubname == clubname) == -1;
 	}
-	createAttendance(clubname: string, interviewers: Interviewer[]): Attendance {
-		let interview = new Attendance(clubname, interviewers);
-		this.attendances.push(interview);
-		return interview;
+	createAttendance(clubname: string, member: any[]): Attendance {
+		let attendance = new Attendance(clubname, member);
+		this.attendances.push(attendance);
+		return attendance;
 	}
 	deleteAttendance(clubname: string): void {
 		this.attendances.splice(
-			this.attendances.findIndex(interviewRoom => interviewRoom.clubname == clubname),
+			this.attendances.findIndex(attendance => attendance.clubname == clubname),
 			1
 		);
 	}
 	getAttendance(clubname: string): Attendance {
-		return this.attendances.find(interviewRoom => interviewRoom.clubname == clubname);
+		return this.attendances.find(attendance => attendance.clubname == clubname);
 	}
 }
 let attendanceManager = new AttendanceManager();
@@ -50,38 +49,38 @@ let attendanceManager = new AttendanceManager();
 const socketRouter: SocketRouter = (io: SocketIO.Server, socket: SocketIO.Socket): void => {
 	socket.on("attendance_createAttendance", (data: clubnameRequest) => {
 		if (!attendanceManager.checkRedundancy(data.clubname)) {
-			socket.emit("attendance_createAttendance", { result: false, message: "이미 생성된 출석입니다." } as InterviewResponse);
+			socket.emit("attendance_createAttendance", { result: false, message: "이미 생성된 출석입니다." } as AttendanceResponse);
 		} else {
-			let interview = attendanceManager.createAttendance(data.clubname, data.interviewers);
+			let attendance = attendanceManager.createAttendance(data.clubname, data.members);
 			socket.leaveAll();
 			socket.join(data.clubname);
-			io.sockets.in(data.clubname).emit("attendance_createAttendance", { result: true, message: "면접 생성 성공", data: interview } as InterviewResponse);
+			io.sockets.in(data.clubname).emit("attendance_createAttendance", { result: true, message: "면접 생성 성공", data: attendance } as AttendanceResponse);
 		}
 	});
 	socket.on("attendance_deleteAttendance", (data: clubnameRequest) => {
 		if (!attendanceManager.checkRedundancy(data.clubname)) {
 			attendanceManager.deleteAttendance(data.clubname);
-			io.sockets.in(data.clubname).emit("attendance_deleteAttendance", { result: true, message: "면접 종료 성공" } as InterviewResponse);
+			io.sockets.in(data.clubname).emit("attendance_deleteAttendance", { result: true, message: "면접 종료 성공" } as AttendanceResponse);
 		} else {
-			socket.emit("attendance_deleteAttendance", { result: false, message: "면접이 없습니다." } as InterviewResponse);
+			socket.emit("attendance_deleteAttendance", { result: false, message: "면접이 없습니다." } as AttendanceResponse);
 		}
 	});
 	socket.on("attendance_getAttendanceByClubName", (data: clubnameRequest) => {
 		socket.leaveAll();
 		socket.join(data.clubname);
 		if (!attendanceManager.checkRedundancy(data.clubname)) {
-			socket.emit("attendance_getAttendanceByClubName", { result: true, message: "면접 가져오기 성공", data: attendanceManager.getAttendance(data.clubname) } as InterviewResponse);
+			socket.emit("attendance_getAttendanceByClubName", { result: true, message: "면접 가져오기 성공", data: attendanceManager.getAttendance(data.clubname) } as AttendanceResponse);
 		} else {
-			socket.emit("attendance_getAttendanceByClubName", { result: false, message: "면접이 없습니다." } as InterviewResponse);
+			socket.emit("attendance_getAttendanceByClubName", { result: false, message: "면접이 없습니다." } as AttendanceResponse);
 		}
 	});
 	socket.on("attendance_updateAttendance", (data: clubnameRequest) => {
 		if (!attendanceManager.checkRedundancy(data.clubname)) {
-			let interview: Attendance = attendanceManager.getAttendance(data.clubname);
-			interview.setInterviewers(data.interviewers);
-			socket.broadcast.in(data.clubname).emit("attendance_updateAttendance", { result: true, message: "면접 갱신 성공", data: interview } as InterviewResponse);
+			let attendance: Attendance = attendanceManager.getAttendance(data.clubname);
+			attendance.setMembers(data.members);
+			socket.broadcast.in(data.clubname).emit("attendance_updateAttendance", { result: true, message: "면접 갱신 성공", data: attendance } as AttendanceResponse);
 		} else {
-			socket.emit("attendance_updateAttendance", { result: false, message: "면접이 없습니다." } as InterviewResponse);
+			socket.emit("attendance_updateAttendance", { result: false, message: "면접이 없습니다." } as AttendanceResponse);
 		}
 	});
 };
