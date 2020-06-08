2229b942895de3f25e5260e6fba017a4f99346e2
Andy0414 <pjh8667@naver.com>
2020-06-08 20:29:55 +0900

    Update keys

---
+++ keys/mail.ts
@@ -0,0 +1,11 @@
+export default {
+	client_id: "",
+	project_id: "",
+	auth_uri: "https://accounts.google.com/o/oauth2/auth",
+	token_uri: "https://oauth2.googleapis.com/token",
+	auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+	client_secret: "",
+	redirect_uris: ["http://localhost:8080", "http://localhost", "https://developers.google.com/oauthplayground"],
+	javascript_origins: ["http://localhost", "http://localhost:8080", "https://circlesapp.kr"],
+	refresh_token: ""
+};
