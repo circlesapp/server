7856867e880f9e2448e8fb657262614bd9de6f30
heptacode <admin@hyunwoo.kim>
2020-10-05 02:14:48 +0900

    Update socket.index.ts

--- src/routers/socket.index.ts
+++ src/routers/socket.index.ts
@@ -1,4 +1,5 @@
-import { Server } from "https";
+// import { Server } from "https";
+import { Server } from "http";
 import * as SocketIO from "socket.io";
 
 import Log from "../modules/Logger";
@@ -9,21 +10,21 @@ import SocketAtendance from "./club/attendance/attendance.socket";
 export type SocketRouter = (io: SocketIO.Server, socket: SocketIO.Socket) => void;
 
 class SocketIOManager {
-	io: SocketIO.Server;
-	socketRouters: SocketRouter[];
-	constructor() {
-		this.socketRouters = [] as SocketRouter[];
-	}
-	use(socketRouter: SocketRouter) {
-		this.socketRouters.push(socketRouter);
-	}
-	start(server: Server) {
-		this.io = SocketIO(server, { origins: "*:*" });
-		this.io.on("connection", socket => {
-			Log.d("SOCKET CONNECT");
-			this.socketRouters.forEach(socketRouter => socketRouter(this.io, socket));
-		});
-	}
+  io: SocketIO.Server;
+  socketRouters: SocketRouter[];
+  constructor() {
+    this.socketRouters = [] as SocketRouter[];
+  }
+  use(socketRouter: SocketRouter) {
+    this.socketRouters.push(socketRouter);
+  }
+  start(server: Server) {
+    this.io = SocketIO(server, { origins: "*:*" });
+    this.io.on("connection", socket => {
+      Log.d("SOCKET CONNECT");
+      this.socketRouters.forEach(socketRouter => socketRouter(this.io, socket));
+    });
+  }
 }
 const socketIOManager = new SocketIOManager();
 socketIOManager.use(SocketInterview);
